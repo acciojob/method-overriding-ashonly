d945bb6c5573e87c73ad1df04cc758f1e73c7d47
ash <ash@ashs-iMac.local>
2023-02-08 19:56:01 +0530

    First ateempt for method overridding assengment of spring1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+  public static class A{
+      public String meth(){
+      return "Invoking method from class A";
+      }
+  }
+  public static class B extends A{
+      @Override
+      public String meth(){
+          return "Method is overridden in Extendend class B";
+      }
+  }
+  public static void main(String[] args) {
+      B objb= new B();
+      objb.meth();
+       B b2 = new B();
+      b2.meth();
+ }
 }
